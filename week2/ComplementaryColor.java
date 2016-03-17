116a3873dbf056e84ac80ea2f3d8c3d2d5e2e437
vymz <vymz@ecomp.poli.br>
2016-03-17 13:23:18 -0300

    Update ComplementaryColor.java
    vymz

--- week2/ComplementaryColor.java
+++ week2/ComplementaryColor.java
@@ -18,38 +18,105 @@ public class ComplementaryColor {
 	 * @param args[1] color (Ex: vermelho)		
 	 */
 	public static void main(String[] args) {
+		// Portuguese		
 		if (args[0].equals("pt")) {
-			String color = args[1];
-			System.out.print("Legal, você escolheu \"" + color);
-			String representation = "";
-			String cColor = "";
-			if ("vermelho".equals(color)) {
-				representation = "tomate";
-				cColor = "verde";
-			}
-			else if ("roxo".equals(color)) {
-				representation = "berinjela";
-				cColor = "amarelo";
-			}
-			else if ("azul".equals(color)) {
-				representation = "mirtilo";
-				cColor = "laranja";
-			}
-			else if ("verde".equals(color)) {
-				representation = "alface";
-				cColor = "vermelho";
-			}
-			else if ("amarelo".equals(color)) {
-				representation = "melão";
-				cColor = "roxo";
-			}
-			else if ("laranja".equals(color)) {
-				representation = "tangerina";
-				cColor = "azul";
-			};
-			System.out.println("\", cor de " + representation + ".");
+			String datos[] = portuguese(args[0], args[1]);
 
-			System.out.println("Sua cor complementar é: " + cColor + "!");
+			System.out.print("Legal, vocÃª escolheu \"" + args[1] );
+			System.out.println("\", cor de " + datos[0] + ".");
+			System.out.println("Sua cor complementar Ã©: " + datos[1] + "!");
+
+			//	System.out.print(data[0]);
+			//	System.out.print(data[1]);
 		}
+
+
+		// English
+		if (args[0].equals("en")) {
+			String data[] = english(args[0], args[1]);
+
+			System.out.print("Great, you chose \"" + args[1] );
+			System.out.println("\", color of " + data[0] + ".");
+			System.out.println("Its complementary color is: " + data[1] + "!");
+		}
+
+
+
+	/* Method for English language*/
+	public static String[] english( String langus, String color ) {
+		String[] data = new String[2];
+		String representation = "";
+		String cColor = "";
+
+		if ("red".equals(color)) {
+			representation = "tomato";
+			cColor = "green";
+		}
+		else if ("purple".equals(color)) {
+			representation = "eggplant";
+			cColor = "yellow";
+		}
+		else if ("blue".equals(color)) {
+			representation = "blueberry";
+			cColor = "orange";
+		}
+		else if ("green".equals(color)) {
+			representation = "lettuce";
+			cColor = "red";
+		}
+		else if ("yellow".equals(color)) {
+			representation = "melon";
+			cColor = "purple";
+		}
+		else if ("orange".equals(color)) {
+			representation = "tangerine";
+			cColor = "blue";
+		}
+
+		data[0] = representation;
+		data[1] = cColor;	
+
+		return data;
+
+	}
+
+	/* Method for Portuguese language*/
+	public static String[] portuguese( String langu, String colores ) {
+		String[] datos = new String[2];
+		String representation = "";
+		String cColor = "";		
+
+		String color = colores;
+
+		if ("vermelho".equals(color)) {
+			representation = "tomate";
+			cColor = "verde";
+		}
+		else if ("roxo".equals(color)) {
+			representation = "berinjela";
+			cColor = "amarelo";
+		}
+		else if ("azul".equals(color)) {
+			representation = "mirtilo";
+			cColor = "laranja";
+		}
+		else if ("verde".equals(color)) {
+			representation = "alface";
+			cColor = "vermelho";
+		}
+		else if ("amarelo".equals(color)) {
+			representation = "melÃ£o";
+			cColor = "roxo";
+		}
+		else if ("laranja".equals(color)) {
+			representation = "tangerina";
+			cColor = "azul";
+		}
+
+		datos[0] = representation;
+		datos[1] = cColor;
+
+		return datos;
+
 	}
 }
